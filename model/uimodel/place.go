48ea516c3486d1190f3bb3f88a87600da3746558
Bogdan Ciric <bogdanciric@Bogdans-MacBook-Pro.local>
2021-02-10 10:48:32 +0100

    coding session changes

--- model/uimodel/place.go
+++ model/uimodel/place.go
@@ -1,12 +1,19 @@
 package uimodel
 
-import "github.com/ciricbogdan/localsearch-home-assignment-backend/model/upstreamAPI"
+import (
+	"fmt"
+	"github.com/ciricbogdan/localsearch-home-assignment-backend/model/upstreamAPI"
+	"strings"
+	"time"
+)
 
 // Place defines the place model which will be sent to the frontend
 type Place struct {
-	Name         string                  `json:"name"`
-	Address      string                  `json:"address"`
-	OpeningHours []DayRangeTimeIntervals `json:"openingHours"`
+	Name          string                  `json:"name"`
+	Address       string                  `json:"address"`
+	OpeningHours  []DayRangeTimeIntervals `json:"openingHours"`
+	Open          bool                    `json:"open"`
+	OpensNextTime string                  `json:"opensNextTime"`
 }
 
 // DayRangeTimeIntervals is a pair of day range (dayA - dayB) and time intervals
@@ -105,7 +112,98 @@ func PlaceFromUpstreamAPI(place upstreamAPI.Place) Place {
 
 	result.OpeningHours = openingHours
 
+	result.Open = checkIfOpen(place)
+
+	if !result.Open {
+		result.OpensNextTime = openNextTime(place)
+	}
+
 	return result
+}
+
+func openNextTime(place upstreamAPI.Place) string {
+
+	now := time.Now().Add(3 * 24 * time.Hour)
+	nowString := fmt.Sprintf("%v:%v", now.Hour(), now.Minute())
+
+	weekDay := now.Weekday() - 1
+	if weekDay == -1 {
+		weekDay = 6
+	}
+
+	currentDay := daysOfTheWeek[weekDay]
+	index := weekDay
+
+	// check for the current day
+	timeIntervals, ok := place.OpeningHours.Days[currentDay]
+	if ok {
+		for _, timeInterval := range timeIntervals {
+			if timeInterval.Start >= nowString {
+				return fmt.Sprintf("today, %v", timeInterval.Start)
+			}
+		}
+	}
+
+	index++
+	for {
+		if index == 7 {
+			index = 0
+		}
+		currentDay := daysOfTheWeek[index]
+		// check for interval
+
+		timeIntervals, ok := place.OpeningHours.Days[currentDay]
+		if ok {
+			resultInterval := timeIntervals[0]
+			return fmt.Sprintf("%v, %v", currentDay, resultInterval.Start)
+		}
+
+		if weekDay == index {
+			break
+		}
+		index++
+	}
+
+	return ""
+}
+
+func checkIfOpen(place upstreamAPI.Place) bool {
+
+	now := time.Now()
+
+	weekDay := now.Weekday() - 1
+	if weekDay == -1 {
+		weekDay = 6
+	}
+
+	currentDay := daysOfTheWeek[weekDay]
+
+	timeIntervals, ok := place.OpeningHours.Days[currentDay]
+	if !ok {
+		return false
+	}
+
+	nowString := fmt.Sprintf("%v:%v", now.Hour(), now.Minute())
+
+	for _, timeInterval := range timeIntervals {
+
+		endTime := timeInterval.End
+
+		if endTime == "00:00" {
+			endTime = "24:00"
+		}
+
+		// 11:30 13:28 14:00
+
+		isAfter := strings.Compare(timeInterval.Start, nowString)
+		isBefore := strings.Compare(endTime, nowString)
+		if isAfter <= 0 && isBefore > 0 {
+			return true
+		}
+
+	}
+
+	return false
 }
 
 // TimeIntervalFromUpstreamAPI converts timeInteval from the upstreamAPI model to the uimodel
